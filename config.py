3be2085bce9d3b8e9a0fd4d10e3b418c0d454be6
BEAST DEVELOPER <beastdeveloper01@gmail.com>
2026-03-04 19:50:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://repgyetdcodkynrbxocg.supabase.co/storage/v1/object/public/images/telegram-1772633965903-8682fd42.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '8055522383').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1003561736092") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1003414875571))
